e592ae8f23db6f4f6a77617dea31a28732571e06
libertea1986 <78866602+libertea1986@users.noreply.github.com>
2022-11-15 17:43:03 +0800

    Create config_server_json.sh

---
+++ config_server_json.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#提升权限
+sudo su
+
+#下载安装包
+wget https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh
+
+#check the download file, install-releash.sh will be shown
+ls -l 
+
+#安装
+install-release.sh > v2ray_install.log
+
+
+# download the server config file
+wget https://github.com/v2fly/v2ray-examples/raw/master/VMess-TCP/config_server.json
+
+#open the file and make some revision
+vim config_server.json
+
+# now you need to modify the config file as follows:
